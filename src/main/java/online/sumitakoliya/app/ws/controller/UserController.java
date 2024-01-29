bbeb76f86880e8cc023a013beb71e1cb2a658188
Sumit <akoliyasumit@hotmail.com>
2024-01-29 19:40:08 +0000

    Extracted path variable from URI

--- src/main/java/online/sumitakoliya/app/ws/controller/UserController.java
+++ src/main/java/online/sumitakoliya/app/ws/controller/UserController.java
@@ -2,17 +2,19 @@ package online.sumitakoliya.app.ws.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users")	//http://localhost:8080/users
+@RequestMapping("/users")	//http://localhost:8080/users
 public class UserController {
-	@GetMapping
-	public String getUser() {
-		return "get user was called";
+	
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "get user was called "+userId;
 	}
 	
 	@PostMapping

de180dd720696c0c013693e31b503244673c4d16
Sumit <akoliyasumit@hotmail.com>
2024-01-30 09:47:18 +0000

    custom error message

--- src/main/java/online/sumitakoliya/app/ws/exceptions/AppExceptionsHandler.java
+++ src/main/java/online/sumitakoliya/app/ws/exceptions/AppExceptionsHandler.java
@@ -1,4 +1,8 @@
 package online.sumitakoliya.app.ws.exceptions;
+import online.sumitakoliya.app.ws.model.response.ErrorMessage;
+
+import java.util.Date;
+
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +17,13 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(value= {Exception.class})
 	public ResponseEntity<Object>handleAnyException(Exception ex, WebRequest request){
+		
+		String errorMessageDescription = ex.getLocalizedMessage();
+		if(errorMessageDescription==null) errorMessageDescription = ex.toString();
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getLocalizedMessage());
+		
 		return new ResponseEntity<>(
-				ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+				errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
